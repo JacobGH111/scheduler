680966ce5233d72e96f439bdc769578a28f1cf68
Jacob Heller <jheller@athenahealth.com>
2017-11-26 20:22:11 -0500

    asdf

--- src/main/java/Scheduler.scala
+++ src/main/java/Scheduler.scala
@@ -1,5 +1,6 @@
-class Room(val capacity:Int, val supportsVideo: Boolean, val supportsSpotlight: Boolean, val supportsPanel: Boolean) {
-  override def toString = s"Room with capacity $capacity"
+case class Room(val capacity:Int, val supportsVideo: Boolean, val supportsSpotlight: Boolean, val supportsPanel: Boolean) {
+//  override def toString = s"Room with capacity $capacity"
+
 
   def satisfies(session: Session):Boolean = {
     (capacity >= session.numPersons) &&
@@ -12,13 +13,12 @@ class Room(val capacity:Int, val supportsVideo: Boolean, val supportsSpotlight: 
 case class Session(val numPersons:Int, val needsVideo: Boolean, val needsSpotlight: Boolean, val needsPanel: Boolean){
   private var a = 5;
 
-  def ana = a;
-  def ana_= (aval: Int):Unit = a = aval
-
   def satisfiedBy(room: Room) = room.satisfies(this)
 
-  override def toString: String = s"Session with $numPersons people"
+//  override def toString: String = s"Session with $numPersons people"
 }
+
+case class Pairing(val session: Session, val room: Room)
 
 object Scheduler{
   def main(args: Array[String]): Unit = {
@@ -28,9 +28,24 @@ object Scheduler{
     val sessions:Array[Session] = Array(
       Session(42, true, false, false),
       Session(47, true, true, true),
+      Session(15, true, false, false),
+      Session(35, false, false, false),
+      Session(54, true, false, true),
+      Session(23, false, true, true)
     )
 
-    println(a == b)
+    val rooms:Array[Room] = Array(
+      Room(62, true, true, true),
+      Room(23, false, true, true),
+      Room(67, false, false, false),
+      Room(23, true, false, true),
+      Room(75, true, true, false),
+      Room(33, true, true, false),
+
+    )
+    println(Room(34,true,true,true).supportsPanel)
+
+
   }
 
 
